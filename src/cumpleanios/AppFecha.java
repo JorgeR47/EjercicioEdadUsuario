d90a2d46d125f9aae1c34450dbecb14ef8e6bd57
JorgeR47 <jorge.revelo@epn.edu.ec>
2024-11-23 16:29:36 -0500

    feat(AppFecha): added new Class

--- src/cumpleanios/AppFecha.java
+++ src/cumpleanios/AppFecha.java
@@ -21,6 +21,7 @@ public class AppFecha {
 
 
         fut2.validarFecha();
+        Libro libro = new Libro("carlos","sanchez","dos");
 
     }
 }
---
+++ src/cumpleanios/Libro.java
@@ -0,0 +1,13 @@
+package cumpleanios;
+
+public class Libro {
+    private String titulo;
+    private String autor;
+    private String editorial;
+
+    public Libro(String titulo, String autor, String editorial) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.editorial = editorial;
+    }
+}
