40b5b5173b1c3bfcf02364e1b03843aac0c20e11
JorgeR47 <jorge.revelo@epn.edu.ec>
2024-11-23 16:19:43 -0500

    feat(Persona): update with other methods in new branch

--- src/cumpleanios/Persona.java
+++ src/cumpleanios/Persona.java
@@ -3,4 +3,8 @@ package cumpleanios;
 public class Persona {
     public Persona(String nombre) {
     }
+
+    public static void main(String[] args) {
+        System.out.println("Nombre: " + args[0]);
+    }
 }
